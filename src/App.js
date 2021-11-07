127643cca65fd8d10bb6c0425f5799b35b830872
Noman <noman@noman.land>
2021-11-07 03:38:44 -0500

    Put 4096 bit number on a 64x64 grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const matrix = Array.from(Array(4096))
+  .map(() => Number(Math.random() > 0.5))
+  .reduce((accum, value, i) => {
+    const tens = Math.floor(i / 64);
+    const ones = i - tens * 64;
+    if (accum[tens]) {
+      accum[tens][ones] = value;
+    } else {
+      accum[tens] = [value];
+    }
+    return accum;
+  }, []);
+
+const App = () => (
+  <div className="app">
+    {matrix.map((y, i) => (
+      <div className="row" key={i}>
+        {y.map((x, j) => (
+          <span className={`cell ${x ? 'on' : ''}`} key={`${i}${j}`}></span>
+        ))}
+      </div>
+    ))}
+  </div>
+);
 
 export default App;
